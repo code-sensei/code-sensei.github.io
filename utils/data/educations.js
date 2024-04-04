f0ca1bd4ff6ca260e5b68c2dad5b91162db07d4a
Babangida Tsowa <tsowababangida@gmail.com>
2024-04-04 12:34:36 +0100

    DONE: Education Section

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,20 +1,20 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    title: "B.Tech Computer Science",
+    duration: "2014 - 2019",
+    institution: "Federal University of Technology, Minna",
   },
-  {
-    id: 2,
-    title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
-  },
-  {
-    id: 3,
-    title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
-  }
+  // {
+  //   id: 2,
+  //   title: "Higher Secondary Certificate",
+  //   duration: "2018 - 2020",
+  //   institution: "Noakhali Islamia Kamil Madrasah",
+  // },
+  // {
+  //   id: 3,
+  //   title: "Secondary School Certificate",
+  //   duration: "2008 - 2018",
+  //   institution: "Baitus Saif Islamia Madrasah",
+  // }
 ]
