b9d8c0e4d1bcbd7bb0fb3c7c2b2cd57fd8b4f81a
Babangida Tsowa <tsowababangida@gmail.com>
2024-04-04 12:21:44 +0100

    DONE: Experience Section

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,50 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer I',
-    company: "Teton Private Ltd.",
-    duration: "(Jan 2022 - Present)"
+    title: 'Chief Technology Officer',
+    company: "Sweet Ride Nigreia Ltd.",
+    duration: "(Oct 2022 - Present)"
   },
   {
     id: 2,
-    title: "FullStack Developer",
-    company: "Fiverr (freelance)",
-    duration: "(Jun 2021 - Jan 2022)"
+    title: "Lead Frontend Engineer",
+    company: "Clafiya",
+    duration: "(Jan 2021 - Oct 2022)"
   },
   {
     id: 3,
-    title: "Self Employed",
-    company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)"
-  }
+    title: "Frontend Developer (Web3, Blockchain) - Contract",
+    company: "Quantum Tech LLC",
+    duration: "(Mar 2022 - Aug 2022)"
+  },
+  {
+    id: 4,
+    title: "Frontend Developer - Contract",
+    company: "Encentral Solutions Ltd.",
+    duration: "(Jul 2021 - Nov 2021)"
+  },
+  {
+    id: 5,
+    title: "Full Stack Developer",
+    company: "Asusu Technology",
+    duration: "(Feb 2020 - Mar 2021)"
+  },
+  {
+    id: 6,
+    title: "Angular Developer - Contract",
+    company: "Unortodox Solutions",
+    duration: "(Jul 2019 - Jan 2020)"
+  },
+  {
+    id: 7,
+    title: "Angular & Ionic Developer - Contract",
+    company: "Asusu Technology",
+    duration: "(Dec 2018 - Mar 2019)"
+  },
+  {
+    id: 8,
+    title: "Frontend Developer Intern",
+    company: "Comsoft Nigeria Ltd.",
+    duration: "(May 2018 - Dec 2018)"
+  },
 ]
